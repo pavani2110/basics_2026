19d327053a833258a7e2e7dd969b689a6c6a822e
pavani2110 <110165072+pavani2110@users.noreply.github.com>
2026-07-22 22:02:39 +0530

    Add files via upload

---
+++ project1/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.get('/', (req, res) => {
+    res.send('hello world', 'utf8');
+});
+app.listen(PORT, () => {
+    console.log(`server running a http://localhost:${PORT}`);
+});
+
